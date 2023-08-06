5f7d5e02f4ab4191b508e43e8e2ab39d576ebb3a
Clément Michaud <clement.michaud34@gmail.com>
2023-08-06 21:41:14 +0200

    embed Dockerfile and docker-compose into binary

--- internal/backupnode/config.go
+++ internal/backupnode/config.go
@@ -10,9 +10,25 @@ import (
 //go:embed config.yml
 var configFile string
 
+//go:embed Dockerfile
+var dockerfile string
+
+//go:embed docker-compose.yml
+var dockerComposeFile string
+
+func dumpFile(path string, content string) {
+	err := os.WriteFile(path, []byte(dockerfile), 0600)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
 func Init() {
 	err := os.WriteFile("config.yml", []byte(configFile), 0600)
 	if err != nil {
 		log.Fatal(err)
 	}
+
+	dumpFile("Dockerfile", dockerfile)
+	dumpFile("docker-compose.yml", dockerComposeFile)
 }
