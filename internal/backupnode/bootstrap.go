365515aa2017062c8d263dc38fca8ccb76426622
Clément Michaud <clement.michaud34@gmail.com>
2023-08-06 14:48:13 +0200

    leave 5 secs to create minio bucket

--- internal/backupnode/bootstrap.go
+++ internal/backupnode/bootstrap.go
@@ -81,10 +81,14 @@ func Initialize(ctx context.Context, configPath string) {
 	bucketName := "any-sync-files"
 	location := "eu-central-1"
 
-	err = minioClient.MakeBucket(ctx, bucketName, minio.MakeBucketOptions{Region: location})
+	newCtx, cancel := context.WithTimeout(ctx, 5*time.Second)
+	defer cancel()
+
+	fmt.Println("Trying to create minio bucket...")
+	err = minioClient.MakeBucket(newCtx, bucketName, minio.MakeBucketOptions{Region: location})
 	if err != nil {
 		// Check to see if we already own this bucket (which happens if you run this twice)
-		exists, errBucketExists := minioClient.BucketExists(ctx, bucketName)
+		exists, errBucketExists := minioClient.BucketExists(newCtx, bucketName)
 		if errBucketExists == nil && exists {
 			log.Printf("We already own %s\n", bucketName)
 		} else {
